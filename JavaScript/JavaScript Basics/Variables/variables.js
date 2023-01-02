67a2689b1ab5a7722bd4824b4fb5eb3feb4d5d96
lufefe98 <lufefezyn@gmail.com>
2023-01-02 15:36:13 +0200

    JavaScript variables practice and notes

---
+++ JavaScript/JavaScript Basics/Variables/variables.js
@@ -0,0 +1,35 @@
+// JavaScript variables are a way to store data that can be used at a later stage.
+// Data can be stored using a statement to declare the variable, the variable name
+// an assignment operator and the value (which is the data that is meant to 
+// be stored)
+
+// var alert = alert("Hello JavaScript")
+
+
+// The Var statement is used to declare the variable, "alert" is the variable name
+// and the equal sign is the "assignment operator" which assigns the variable name
+// "alert" to the value on the opposite side of the operator.
+
+// let alert = alert("Hello world")
+
+// The above case will not work only because the example used above involves an
+// alert, but normaly when working with other types of data such as numbers and
+// strings, it is possible to assign a new value to a variable name associated
+// with let or var
+
+// The let statement is an updated version of var which is part of ES6 JavaScript
+// and it differs from Var in that it is only accessible within the variable that
+// it is only available inside the block it is defined within, where as var can be
+// accessed throughout the function
+
+const userPrompt = prompt("Type your name in here")
+// Const is another way of declaring a variable and is different from let and var
+// in that it can not be reassigned/updated to another value
+
+// Const is similar to let in that it is only accessible within the block it is
+// declared within.
+
+
+
+// Variables can be recalled at a later stage by invoking their name in objects, logic
+// statements etc.
