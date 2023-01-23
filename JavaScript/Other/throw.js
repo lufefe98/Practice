718012c4c857ede67ab1f579c6d057df0f78131f
lufefe98 <lufefezyn@gmail.com>
2023-01-23 11:43:06 +0200

    Practiced using throw statements and left notes

--- JavaScript/Other/throw.js
+++ JavaScript/Other/throw.js
@@ -1,1 +1,19 @@
 // Throw Statements
+function getArrLength (arr) {
+    if (arr.length > 5) {
+        throw new Error("Too many items")
+    }
+}
+
+try {
+    getArrLength([1, 2, 3, 4, 5, 6])
+} catch (err) {
+    console.log(err)
+}
+
+// Throw statements allow for the creation of user-defined exceptions (ie: an exception that is defined
+// by the developer which would not necessarily be an error coming from JavaScript).
+
+// Side note: Any statements that come after "throw" will not be executed and the function will stop and
+// then control of the execution will pass on to the catch block and if there is no catch block, then the
+// execution will terminate.
