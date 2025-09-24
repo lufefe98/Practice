167a10610f06b3fdaa5e8dbede2433cd0300a538
lufefe98 <lufefezyn@gmail.com>
2025-09-24 18:44:07 +0200

    Add sorting algorithm for sorting project

--- JavaScript/Projects/ChatGptAlgoPractice/SortingAlgoProject/app.js
+++ JavaScript/Projects/ChatGptAlgoPractice/SortingAlgoProject/app.js
@@ -1,30 +1,75 @@
 // Select Elements
 
-const card = document.querySelectorAll('.card')
-const productName = document.querySelectorAll('.product-name')
-const productPrice = document.querySelectorAll('.product-price')
-const dropDownMenu = document.querySelector('select')
 
 
 // Access Price and Product Names
 
-for (let i = 0; i < card.length; i++) {
-    const productName= card[i].childNodes[3].childNodes[1].textContent
-    const productPrice= card[i].childNodes[3].childNodes[3].textContent
 
-    console.log({productName, productPrice})
-}
-
-
-
-
-// Create Array/Map
-
-
-
-
-// Sorting functions
 
 
 
 // Event listeners
+
+// dropDownMenu.addEventListener('change', function () {
+//     const selected = this.value
+//     // Decide how to sort here
+
+//     let products = card.map(getProductValues)
+
+//     if (selected === 'price-asc') {
+//         for (let i = 0; i < products.length; i++) {
+//             if () { }
+//         }
+//     }
+
+//     if (selected === 'price-desc') {
+//         console.log('cash')
+//     }
+
+//     if (selected === 'name-asc') {
+
+//     }
+
+//     if (selected === 'name-desc') {
+
+//     }
+
+// })
+
+
+
+const container = document.querySelector('.product-container')
+const dropDownMenu = document.querySelector('select')
+
+dropDownMenu.addEventListener('change', function () {
+  const cards = Array.from(container.querySelectorAll(".card"));
+
+  // Build array of objects: { card, name, price }
+  const cardData = cards.map(card => {
+    const name = card.querySelector(".product-name").textContent.toLowerCase();
+    const price = parseFloat(
+      card.querySelector(".product-price").textContent.replace(/[^0-9.]/g, "")
+    );
+    return { card, name, price };
+  });
+
+// Sort depending on selection
+  switch (dropDownMenu.value) {
+    case "price-asc":
+      cardData.sort((a, b) => a.price - b.price);
+      break;
+    case "price-desc":
+      cardData.sort((a, b) => b.price - a.price);
+      break;
+    case "name-asc":
+      cardData.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case "name-desc":
+      cardData.sort((a, b) => b.name.localeCompare(a.name));
+      break;
+  }
+
+  // Re-render cards
+  container.innerHTML = "";
+  cardData.forEach(item => container.appendChild(item.card));
+});
