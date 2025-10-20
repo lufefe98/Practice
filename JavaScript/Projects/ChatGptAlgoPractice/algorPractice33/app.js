29d267c4bd0baf59cb6a753c8eeb4399aa584b13
lufefe98 <lufefezyn@gmail.com>
2025-10-21 01:03:16 +0200

    Add linear scan algorithm practice 2 with javascript solution skeleton

---
+++ JavaScript/Projects/ChatGptAlgoPractice/algorPractice33/app.js
@@ -0,0 +1,15 @@
+// Arrays
+
+const numArray = [1, 2, 3, 4, 2, 5, 1, 6, 7, 3]
+
+const sortedNumArray = numArray.sort((a, b) => a - b)
+
+
+function removeDuplicate(arr, noDupeArr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (i = 1 || arr[i] !== arr[i -1]) {
+
+        }
+    }
+    return 
+}
