f38e353da7a6a46f6fc9ff07ffb3a42498d2c1a8
lufefe98 <lufefezyn@gmail.com>
2025-10-21 02:22:40 +0200

    Add sorted linear scan algorithm practice

--- JavaScript/Projects/ChatGptAlgoPractice/algorPractice33/app.js
+++ JavaScript/Projects/ChatGptAlgoPractice/algorPractice33/app.js
@@ -3,13 +3,16 @@
 const numArray = [1, 2, 3, 4, 2, 5, 1, 6, 7, 3]
 
 const sortedNumArray = numArray.sort((a, b) => a - b)
+const noDuplicateArray = []
 
 
 function removeDuplicate(arr, noDupeArr) {
     for (let i = 0; i < arr.length; i++) {
-        if (i = 1 || arr[i] !== arr[i -1]) {
-
+        if (i === 0 || arr[i] !== arr[i - 1]) {
+            noDupeArr.push(arr[i])
         }
     }
-    return 
+    return noDupeArr
 }
+
+console.log(removeDuplicate(sortedNumArray, noDuplicateArray))
