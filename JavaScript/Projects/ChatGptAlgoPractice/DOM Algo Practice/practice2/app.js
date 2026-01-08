ae0447c88a31d5a8de93ecfdee3e073b717a1f6e
lufefe98 <lufefezyn@gmail.com>
2026-01-09 00:00:29 +0200

    Add selection of DOM elements

--- JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice2/app.js
+++ JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice2/app.js
@@ -1,8 +1,11 @@
 
 // DOM Elements
 
+const cardContainer = document.getElementById('cards-container')
+const cards = document.querySelectorAll('.card')
 
-
+const priceBtn = document.getElementById('price-sort')
+const nameBtn = document.getElementById('name-sort')
 
 
 // Functions
