11717b1963aec58728db53b9c13022d5705087f3
lufefe98 <lufefezyn@gmail.com>
2026-01-13 22:26:30 +0200

    Add new sorting practice files

---
+++ JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice3/app.js
@@ -0,0 +1,30 @@
+// Select elements
+
+const container = document.getElementById('cards-container')
+const cards = Array.from(document.querySelectorAll('.cards'))
+
+const priceBtn = document.getElementById('price-sort')
+const nameBtn = document.getElementById('name-sort')
+
+
+// Functions
+
+function sortByPrice() {
+    
+}
+
+
+
+
+function sortByName() {
+
+}
+
+
+
+
+// Event Listeners
+
+priceBtn.addEventListener(click, sortByPrice)
+
+nameBtn.addEventListener(click, sortByName)
