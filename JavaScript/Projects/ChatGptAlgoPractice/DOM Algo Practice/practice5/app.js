75cdd4f29bfed025c3571161d04fa843270f99c9
lufefe98 <lufefezyn@gmail.com>
2026-01-24 16:11:34 +0200

    Add filter products by search

--- JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice5/app.js
+++ JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice5/app.js
@@ -1,11 +1,48 @@
 // Select Elements
 
+const cards = Array.from(document.querySelectorAll('.card'))
+const container = document.getElementById('cards-container')
+const noMatchText = document.getElementById('no-match-text')
+
+const searchInput = document.getElementById('text')
+const rangeInput = document.getElementById('range-slider')
 
 
 
 // Functions
 
+function getSearchValue(event) {
+    const searchVaue = event.target.value.toLowerCase().trim()
+    let hasMatch = false
+
+
+    cards.forEach(card => {
+        const productName = card.querySelector('.prod-name').textContent
+
+        if (productName.includes(searchVaue)) {
+            card.style.display = ''
+            hasMatch = true
+        } else {
+            card.style.display = 'none'
+        }
+
+    });
+
+    noMatchText.style.display = hasMatch ? 'none' : 'block'
+}
+
+
+// function getRangeValue(event) {
+
+// Prevent min slider from passing max slider
+
+// }
 
 
 
-// Event Listeners
+
+// // Event Listeners
+
+searchInput.addEventListener('input', getSearchValue)
+
+// rangeInput.addEventListener('input', getRangeValue)
