b9418a8cc7aec1a069e78952cf434a427e47ba36
lufefe98 <lufefezyn@gmail.com>
2026-01-26 22:15:26 +0200

    Add sliding window algorithm practice

---
+++ JavaScript/Projects/ChatGptAlgoPractice/algoPractice71/app.js
@@ -0,0 +1,14 @@
+// Array
+
+const array = [2, 4, 3, 1, 5, 8, 6, 9, 7]
+
+// Function
+
+// find subarray with largest value and return the number
+
+function slidingWindow(arr) {
+
+
+}
+
+console.log(slidingWindow(array))
