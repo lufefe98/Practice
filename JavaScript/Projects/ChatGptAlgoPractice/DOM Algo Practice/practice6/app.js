a0893bef1336744106b763ca94f6d7424547c2c8
lufefe98 <lufefezyn@gmail.com>
2026-01-26 14:16:56 +0200

    Add product price and name filters

--- JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice6/app.js
+++ JavaScript/Projects/ChatGptAlgoPractice/DOM Algo Practice/practice6/app.js
@@ -1,5 +1,15 @@
 // Select Elements
 
+const cards = Array.from(document.querySelectorAll('.card'))
+const container = document.getElementById('cards-container')
+const noMatchText = document.getElementById('no-match-text')
+
+const searchInput = document.getElementById('text')
+const minRange = document.getElementById('fromSlider')
+const maxRange = document.getElementById('toSlider')
+
+// const sortMenu = document.querySelector('#sort-menu')
+
 
 
 
@@ -7,14 +17,52 @@
 
 // Functions 
 
+function filterProducts() {
+    // Input Values
+    const searchValue = searchInput.value.toLowerCase().trim()
+    const minPrice = Number(minRange.value)
+    const maxPrice = Number(maxRange.value)
 
 
+    // Matching Checks
+
+    let hasMatch = false
+
+    cards.forEach(card => {
+        // Card Values
+        const productName = card.querySelector('.prod-name').textContent
+        const productPrice = Number(card.querySelector('.price').textContent)
+
+        // Filter Booleans
+        const priceMatch = productPrice >= minPrice && productPrice <= maxPrice
+        const searchMatch = searchValue === '' || productName.includes(searchValue)
+        
+
+        // Filter Products
+        if (searchMatch && priceMatch) {
+            card.style.display = ''
+            hasMatch = true
+        } else {
+            card.style.display = 'none'
+        }
+    });
+
+    noMatchText.style.display = hasMatch ? 'none' : 'block'
+}
+
+
+// function sortProducts() {
+
+// }
 
 
 
 // Event Listeners
 
+searchInput.addEventListener('input', filterProducts)
 
+minRange.addEventListener('input', filterProducts)
+maxRange.addEventListener('input', filterProducts)
 
 
 
