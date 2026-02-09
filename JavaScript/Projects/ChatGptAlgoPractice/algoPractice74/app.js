ac425d3d1ddc619aac80ef98725caadc1b4d3f0d
lufefe98 <lufefezyn@gmail.com>
2026-02-09 23:56:20 +0200

    Add find first target value sliding window algorithm

--- JavaScript/Projects/ChatGptAlgoPractice/algoPractice74/app.js
+++ JavaScript/Projects/ChatGptAlgoPractice/algoPractice74/app.js
@@ -1,4 +1,8 @@
-// String
+// Array
+
+const array = [1, 4, 7, 3, 2, 5, 9, 6, 12]
+
+const k = 3
 
 
 
@@ -6,9 +10,34 @@
 // Function
 
 
+function firstTarget(arr) {
+    if (k <= 0 || k > arr.length) return 'Incorrect window size'
+
+    let sum = 0
+    let left = 0
+    let right = k
+    const target = 15
+    
+    for (let i = 0; i < k; i++) {
+        sum += arr[i]
+        
+        while (sum < target) {
+           sum -= arr[left]
+           sum += arr[right]
+
+           left++
+           right++
+
+        }
+        
+    }
+    if (sum >= target) {
+        return sum
+    }
+}
+
 
 
 // Execute
 
-
-
+console.log(firstTarget(array))
